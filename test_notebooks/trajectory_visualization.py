249078cec190cb24f29b678d99799c7817f56e42
Chad Curtis <ccurtis7@uw.edu>
2016-02-22 12:32:17 -0800

    Added a test file and added a new test file.

--- test_notebooks/trajectory_visualization.py
+++ test_notebooks/trajectory_visualization.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def download_trajectory_data(file):
     """
-    Downloads a specified datafile and converts to a numpy array.  If the file
+    Converts a .csv file dataset to a numpy array.  If the file
     is not a .csv file or the file does not exist, returns an error message.
 
     Inputs: .csv file.
@@ -24,15 +24,17 @@ def download_trajectory_data(file):
             name = np.genfromtxt(file, delimiter=",")
             name = np.delete(name, 0, 0)
         except:
-            return 'File does not exist'
+            print('File does not exist')
             file_exists = False
+            return (file_csv, file_exists)
         else:
             return name
 
     else:
 
         file_csv = False
-        return 'File is not a .csv file'
+        print('File is not a .csv file')
+        return (file_csv, file_exists)
 
 
 def define_xydata(dataset, sets):
@@ -55,13 +57,47 @@ def define_xydata(dataset, sets):
             trajectory arrays ((n-1)/3 columns)
     """
 
-    times = dataset[:, 0]
-    Run = dict()
+    splitsuccess = True
+    wholenumber = True
+    justrightsets = True
+    correctformat = True
 
-    for num in range(1, sets + 1):
-        Run["Run" + str(num)] = dataset[:, 3 * num - 2: 3 * num + 1]
+    # Check that dataset is of the correct format.
+    if (dataset.shape[1] - 1) % 3 == 0:
 
-    return times, Run
+        # Check that the desire number of sets matches the given dataset.
+        if sets < (dataset.shape[1] - 1)/3:
+
+            # Check that the desired number of sets is a whole number.
+            if float(sets).is_integer():
+
+                # Define the times column.
+                times = dataset[:, 0]
+                Run = dict()
+
+                # Build individual trajectory datasets.
+                for num in range(1, sets + 1):
+                    Run["Run" + str(num)] = dataset[:, 3 * num - 2: 3 * num + 1]
+
+                return times, Run
+
+            else:
+                print("The variable 'sets' must be a whole number.")
+                wholenumber = False
+                splitsuccess = False
+                return (splitsuccess, wholenumber, justrightsets, correctformat)
+
+        else:
+            print("Desired number of sets is too high for this dataset.")
+            justrightsets = False
+            splitsuccess = False
+            return (splitsuccess, wholenumber, justrightsets, correctformat)
+
+    else:
+        print("Data is not of right format.  Dataset must have 1 time column and sets of 3 columns of trajectory data.")
+        correctformat = False
+        splitsuccess = False
+        return (splitsuccess, wholenumber, justrightsets, correctformat)
 
 
 def shift_trajectory(xydata):
