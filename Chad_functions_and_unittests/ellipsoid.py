d3360c1df1d550c75cd58c2c3364acbeead46ae3
Chad Curtis <ccurtis7@uw.edu>
2016-07-27 17:59:19 -0700

    Built new function plot_mveeoverl2

--- Chad_functions_and_unittests/ellipsoid.py
+++ Chad_functions_and_unittests/ellipsoid.py
@@ -347,6 +347,101 @@ def plot_mveeoverl(traj, n1, n2, p, q, scale, dec, filename, limit):
     plt.savefig('{}.png'.format(filename), bbox_inches='tight')
 
 
+def plot_mveeoverl2(traj, n1, n2, p, q, scale, dec, filename, limit):
+    """
+    This function creates a single 3D plot from trajectory data.  This dataset
+    must include a column of particle numbers as well as the x, y, and z
+    coordinates of of each particle at each frame. Output will be saved as a
+    .png file of the desired name.
+    Inputs:
+    traj: array of trajectory data e.g. particle #, frames, x, y, z, Deff, MSD
+    n1: particle# column
+    n2: xyz data start (so x data column, 29 for a normal dataset)
+    a range)
+    dec: how many decimals you would like to be displayed in the graph.
+    filename: what you want to name the file.  Must be in ''.
+    limit defines the range of z, y, and z.
+    Can also use plt.show() afterwards to preview your data, even if it skews the title and legend a bit.
+
+    This differs from plot_mveeoverl in that it excludes the maxtraj function.
+    The ellipse is based off ALL points in the dataset.  This is good for small
+    er datasets.
+    """
+
+    # Creates an array 'particles' that contains the particle number at each frame.
+    particles = traj[:, n1]
+    position = traj[:, n2:n2+4]
+    total = int(max(particles))
+    total1 = total + 1
+    path = dict()
+
+    # Creates an array for each trajectory containing all xyz data
+    for num in range(1, total1):
+
+        hold = np.where(particles == num)
+        itindex = hold[0]
+        min1 = min(itindex)
+        max1 = max(itindex)
+        path[num] = (position[min1:max1, :])
+
+    # but = maxtraj(traj, n1, n2, p, q)
+
+    pi = np.pi
+    sin = np.sin
+    cos = np.cos
+
+    A, centroid = mvee(position)
+    A = scale * A
+    U, D, V = la.svd(A)
+    rx, ry, rz = 1./np.sqrt(D)
+    u, v = np.mgrid[0:2*pi:20j, -pi/2:pi/2:10j]
+
+    def ellipse(u, v):
+        x = rx*cos(u)*cos(v)
+        y = ry*sin(u)*cos(v)
+        z = rz*sin(v)
+        return x, y, z
+
+    E = np.dstack(ellipse(u, v))
+    E = np.dot(E, V) + centroid
+    x, y, z = np.rollaxis(E, axis=-1)
+
+    # Creates figure
+    fig = plt.figure(figsize=(24, 18), dpi=80)
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_title('Particle Trajectories', x=0.5, y=1.15)
+
+    # Plots individual trajectories
+    for num in range(1, total1):
+
+        ax.plot(path[num][:, 0], path[num][:, 1], path[num][:, 2], label='Particle {}'.format(num))
+
+    ax.plot_surface(x, y, z, cstride=1, rstride=1, alpha=0.05)
+
+    axbox = ax.get_position()
+    ax.legend(loc=(0.86, 0.90), prop={'size': 20})
+    ax.locator_params(nbins=6)
+    ax.view_init(elev=38, azim=72)
+
+    plt.gca().set_xlim([-limit, limit])
+    plt.gca().set_ylim([-limit, limit])
+    plt.gca().set_zlim([-limit, limit])
+
+    for item in ([ax.xaxis.label, ax.yaxis.label, ax.zaxis.label] +
+                 ax.get_xticklabels() + ax.get_yticklabels() + ax.get_zticklabels()):
+        item.set_fontsize(13)
+
+    ax.title.set_fontsize(35)
+    ax.tick_params(direction='out', pad=16)
+    plt.gca().xaxis.set_major_formatter(mpl.ticker.FormatStrFormatter('%.{}f um'.format(dec)))
+    plt.gca().yaxis.set_major_formatter(mpl.ticker.FormatStrFormatter('%.{}f um'.format(dec)))
+    plt.gca().zaxis.set_major_formatter(mpl.ticker.FormatStrFormatter('%.{}f um'.format(dec)))
+
+    plt.show()
+    # Save your figure
+    plt.savefig('{}.png'.format(filename), bbox_inches='tight')
+
+
 def rotmat(traj, n1, n2, A):
     """
     A similar function to roti, this function will rotate any given three-coor
