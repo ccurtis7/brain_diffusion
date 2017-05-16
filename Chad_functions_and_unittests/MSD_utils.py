043069347ece869b9f60b77178c611a04cca0b8b
Chad Curtis <ccurtis7@uw.edu>
2017-05-16 13:38:19 -0700

    Added new function to build time arrays.

--- Chad_functions_and_unittests/MSD_utils.py
+++ Chad_functions_and_unittests/MSD_utils.py
@@ -33,3 +33,21 @@ def get_data(channels, surface_functionalities, media, concentrations, replicate
                                                           delimiter=",")
 
     return data
+
+
+def build_time_array(frames=90, conversion=(0.16, 9.89, 1), SD_frames=[1, 7, 14]):
+    """
+    Builds (1) a time array based on the desire number of frames and the fps and (2) a shortened time array at which the
+    standard deviations are going to be calculated.
+
+    The default is 100 frames, a conversion factor of 0.16 microns per pixel, 9.89 frames per second, and 1 micron per z-stack.
+    """
+
+    frames_1 = np.linspace(0, frames, frames+1).astype(np.int64)
+    time = frames_1/conversion[1]
+    time_SD = np.zeros(np.size(SD_frames))
+
+    for i in range(0, np.size(SD_frames)):
+        time_SD[i] = time[SD_frames[i]]
+
+    return time, time_SD
