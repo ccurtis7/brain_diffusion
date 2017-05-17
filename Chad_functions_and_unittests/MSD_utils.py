8639aac6c04ce5494b03c3c3ea796f19274047d6
Chad Curtis <ccurtis7@uw.edu>
2017-05-17 09:16:01 -0700

    Added averaging tools.

--- Chad_functions_and_unittests/MSD_utils.py
+++ Chad_functions_and_unittests/MSD_utils.py
@@ -21,10 +21,16 @@ def get_data(channels, surface_functionalities, media, concentrations, replicate
     """
 
     data = {}
+    avg_sets = {}
+    counter = 0
+
     for channel in channels:
         for surface_functionality in surface_functionalities:
             for medium in media:
                 for concentration in concentrations:
+                    test_value = "{}_{}_{}_{}".format(channel, surface_functionality, medium, concentration)
+                    avg_sets[counter] = test_value
+                    counter = counter + 1
                     for replicate in replicates:
                         sample_name = "{}_{}_{}_{}_{}".format(channel, surface_functionality, medium, concentration, replicate)
                         filename = path.format(channel=channel, surface_functionality=surface_functionality, medium=medium,
@@ -32,7 +38,7 @@ def get_data(channels, surface_functionalities, media, concentrations, replicate
                         data[sample_name] = np.genfromtxt(filename,
                                                           delimiter=",")
 
-    return data
+    return data, avg_sets
 
 
 def build_time_array(frames=90, conversion=(0.16, 9.89, 1), SD_frames=[1, 7, 14]):
@@ -41,6 +47,9 @@ def build_time_array(frames=90, conversion=(0.16, 9.89, 1), SD_frames=[1, 7, 14]
     standard deviations are going to be calculated.
 
     The default is 100 frames, a conversion factor of 0.16 microns per pixel, 9.89 frames per second, and 1 micron per z-stack.
+
+    Example:
+    build_time_array(90, (0.16, 9.89, 1), [1, 7, 14, 15])
     """
 
     frames_1 = np.linspace(0, frames, frames+1).astype(np.int64)
@@ -51,3 +60,31 @@ def build_time_array(frames=90, conversion=(0.16, 9.89, 1), SD_frames=[1, 7, 14]
         time_SD[i] = time[SD_frames[i]]
 
     return time, time_SD
+
+
+def return_average(to_average='YG_nPEG_in_agarose_1x'):
+    """
+    Averages over replicates within a sample.
+
+    Parameters:
+    to_average is a string.
+
+    Example:
+    return_average('RED_PEG_in_agarose_10x')
+    """
+
+    to_avg = {}
+    counter = 0
+    for keys in data:
+        if to_average in keys:
+            to_avg[counter] = keys
+            counter = counter + 1
+
+    to_avg_num = np.zeros((frames, counter))    
+    for i in range(0, counter):
+        for j in range(0, frames):
+            to_avg_num[j, i] = data[to_avg[i]][j]
+
+    answer = np.mean(to_avg_num, axis=1)
+
+    return answer
