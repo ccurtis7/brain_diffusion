c2d6ff6a5550235b69077ff85ccea031aa4c75e1
Chad Curtis <ccurtis7@uw.edu>
2017-05-24 15:33:52 -0700

    Fixed small error.

--- Chad_functions_and_unittests/MSD_utils.py
+++ Chad_functions_and_unittests/MSD_utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import numpy.ma as ma
 
 
 def get_data_gels(channels, surface_functionalities, media, concentrations, replicates, path):
